8419a20db0ed3c162eedb2bf21e366a32cd69cb5
sakio87 <guilherme-oyama87@hotmail.com>
2025-08-06 14:05:52 +0100

    Atualizações no admin, views, settings e templates

--- blog/admin.py
+++ blog/admin.py
@@ -1,12 +1,11 @@
 from django.contrib import admin
-
-# Register your models here.
 from .models import Post
 
 class PostAdmin(admin.ModelAdmin):
-    list_display = ("title","slug","status","created_on")
+    prepopulated_fields = {"slug": ("title",)}
+    list_display = ("title", "slug", "status", "created_on")
     list_filter = ("status",)
-    search_fields = ["title","content"]
-    prepopulated_fields = {"slug":("title",)}
+    search_fields = ["title", "content"]
 
-admin.site.register(Post)
+admin.site.register(Post, PostAdmin)
+
--- blog/views/__init__.py
+++ blog/views/__init__.py
@@ -1,6 +1,6 @@
 # blog/views/__init__.py
 from .post_view import PostView
-
+from.post_view import PostDetail
 __all__ = ['PostView']
 
 
