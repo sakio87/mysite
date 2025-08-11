119952530b759b2305d018f2025e1259104b0853
sakio87 <guilherme-oyama87@hotmail.com>
2025-08-11 23:50:24 +0100

    Atualizações em settings.py e templates

--- mysite/settings.py
+++ mysite/settings.py
@@ -60,7 +60,7 @@ ROOT_URLCONF = 'mysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [TEMPLATES_DIR],
+        'DIRS': [BASE_DIR / "templates"],
 
         'APP_DIRS': True,
         'OPTIONS': {
